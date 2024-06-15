de96c923f28fcf113bee81b0a1ccb2e005fda09a
星星來囉 <152477789+ijk0906148030@users.noreply.github.com>
2024-06-15 23:34:27 +0800

    Update train_aux.py

--- yolov7/train_aux.py
+++ yolov7/train_aux.py
@@ -525,7 +525,7 @@ def train(hyp, opt, device, tb_writer=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default='yolov7/pretrained/yolov7-w6_training.pt', help='initial weights path') ##
+    parser.add_argument('--weights', type=str, default='yolov7/pretrained/yolov7-w6.pt', help='initial weights path') ##
     parser.add_argument('--cfg', type=str, default='yolov7/cfg/training/yolov7-w6-AICUP.yaml', help='model.yaml path') ##
     parser.add_argument('--data', type=str, default='yolov7/data/AICUP.yaml', help='data.yaml path') ##
     parser.add_argument('--hyp', type=str, default='yolov7/data/hyp.scratch.p5.yaml', help='hyperparameters path')
